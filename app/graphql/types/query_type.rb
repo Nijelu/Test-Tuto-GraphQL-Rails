8d688cbe7366063a977dbed9cde6678924816cc8
Julien Ducrocq <julien.ducrocq77@gmail.com>
2018-11-13 15:42:29 +0100

    Final / New Syntax Fail / Full OLD syntax

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,3 +1,33 @@
+# class Types::QueryType < Types::BaseObject
+#   graphql_name 'Query'
+  
+#   ### TODO LIST QUERY
+#   field :todo_lists, [Types::TodoListType], null: true do
+#     description 'returns all todo lists'
+#   end
+#   field :todo_list, Types::TodoListType, null: true do
+#     description 'returns the queried todo list'
+#     argument :id, ID, required: true
+#   end
+  
+#   def todo_lists
+#     TodoList.all
+#   end
+  
+#   def todo_list(args)
+#     TodoList.find_by!(id: args[:id])
+#   end
+  
+#   ### ITEM QUERY
+#   field :items, [Types::ItemType], null: true do
+#     description 'returns all items'
+#   end
+  
+#   def items
+#     Item.all
+#   end
+# end
+
 module Types
   QueryType = GraphQL::ObjectType.new.tap do |root_type|
     root_type.name = 'Query'
@@ -9,3 +39,23 @@ module Types
     ])
   end
 end
+  
+
+# class Types::QueryType < GraphQL::Schema::Object
+#   field :projects, [Types::ProjectType], null: false do
+#     description 'Find all the projects'
+#   end
+
+#   field :project, Types::ProjectType, null: true do
+#     description 'Find a project by slug'
+#     argument :slug, String, required: true
+#   end
+
+#   def projects
+#     Project.all
+#   end
+
+#   def project(args)
+#     Project.find_by(slug: args[:slug])
+#   end
+# end
--- spec/graphql/types/todo_list_type_spec.rb
+++ spec/graphql/types/todo_list_type_spec.rb
@@ -1,7 +1,16 @@
+# require_relative '../../../app/graphql/types/todo_list_type.rb'
+
+
 RSpec.describe Types::TodoListType do
     # avail type definer in our tests
     types = GraphQL::Define::TypeDefiner.instance
-
+    # p protected_methods = Types::TodoListType.protected_instance_methods
+    # before(:each) do
+    #     Types::TodoListType.send(:public, *protected_methods)  
+    # end
+    # after(:each) do
+    #     Types::TodoListType.send(:protected, *protected_methods)  
+    # end
     it 'has an :id field of ID type' do
         # ensure that the field id is of type ID
         expect(subject).to have_field(:id).that_returns(!types.ID)
@@ -11,4 +20,5 @@ RSpec.describe Types::TodoListType do
         # ensure the field is of String type
         expect(subject).to have_field(:title).that_returns(!types.String)
     end
+
 end
