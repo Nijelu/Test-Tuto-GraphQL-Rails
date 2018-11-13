030c591c4d2d9b64003ba0cbd12d14ef07d843db
Julien Ducrocq <julien.ducrocq77@gmail.com>
2018-11-13 09:39:07 +0100

    Last commit with OLD synthax

---
+++ spec/graphql/mutations/todo_list_mutation_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Mutations::TodoListMutation do
+    describe 'creating a ne record' do
+        let(:args) do
+            {
+                title: 'Some random title'
+            }
+        end
+
+        it 'increases todo lists by 1' do
+            mutation = subject.fields['create_todo_list'].resolve(nil, args, nil)
+            #  adds one todo_list to the db
+            expect(mutation).to change { TodoList.count }.by 1
+        end
+    end
+end
